a607864ff7ceb91acbfdd7048c54cce70093a896
Ifan <ifanmo@gmail.com>
2023-11-08 11:17:32 +0000

    Added authentication for endpoints

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import MenuItemsView, SingleMenuItemView, index
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', index, name='index'),
     path('menu/', MenuItemsView.as_view(), name='menu_items'),
     path('menu/<int:pk>', SingleMenuItemView.as_view(), name='menu_item'),
-    
+    path('api-token-auth/', obtain_auth_token),
 ]
--- Restaurant/views.py
+++ Restaurant/views.py
@@ -2,20 +2,25 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Booking, Menu
 from .serializers import BookingSerializer, MenuSerializer
 def index(request):
     return render(request, 'index.html', {})
-  
+ 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
 class MenuItemsView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
     
 class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
