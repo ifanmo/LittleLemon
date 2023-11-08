77ef3f5994d0b6a3f0638ab02902383561005910
Ifan <ifanmo@gmail.com>
2023-11-08 13:02:28 +0000

    added unit tests

--- Restaurant/tests.py
+++ Restaurant/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Menu
 
 # Create your tests here.
+class MenuItemTest(TestCase):
+    def test_get_item(self):
+        item = Menu.objects.create(title='Cake', price=80, inventory=100)
+        self.assertEqual(item, "Cake : 80")
